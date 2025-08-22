3621b1be58610bb9019c6c61d96140da72b10e33
Zhu Jiale <Jiale.Zhu@amd.com>
2025-08-23 00:32:57 +0800

    fix numeric error

--- rdma_monitor/monitor_rx.cpp
+++ rdma_monitor/monitor_rx.cpp
@@ -3,6 +3,7 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 #include <unistd.h>
@@ -12,6 +13,8 @@
 #include <cstdlib>
 #include <csignal>
 #include <atomic>
+#include <ranges>
+#include <numeric>
 #include "utils.h"
 
 std::atomic<bool> exit_requested(false);
@@ -19,8 +22,15 @@ std::atomic<bool> exit_requested(false);
 using namespace std::literals::chrono_literals;
 namespace fs = std::filesystem;
 #define NOW (std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count())
-#define MAX_SAMPLE (10000)
+#define MAX_SAMPLE (2000000)
 #define SAMPLE_INTERVAL (1000) // ns
+
+struct RawProfilePoint{
+    uint64_t timestamp;
+    uint64_t bytes;
+    double bw; //gbs
+};
+
 
 void signal_handler(int signal) {
     if (signal == SIGINT) {
@@ -60,16 +70,77 @@ void profile_nic_rx(std::string nic_name){
         counts++;
         std::this_thread::sleep_for(10us);
     }
-    std::fstream ofs(std::string("data_")+nic_name+std::string("_rx.txt"), std::ios::out | std::ios::trunc);
+    std::string file_name = std::string("data_")+nic_name+std::string("_rx.txt");
+    std::fstream ofs(file_name, std::ios::out | std::ios::trunc);
     if(ofs){
         for(int i=0;i<MAX_SAMPLE;i++){
             ofs<<ts[i]<<","<<bytes[i]<<"\n";
         }
     }
     ofs.close();
+
+    // 对于已经写入的数据文件,删除其中重复的rx列的数据
+    std::ifstream ifs(file_name);
+    if (!ifs.is_open()) {
+        std::cerr << "Error: Failed to open file " << file_name << std::endl;
+        throw std::runtime_error("Error: Failed to open file " + file_name);
+    }
+
+    std::vector<RawProfilePoint> profile_data;
+    std::string line;
+    while(std::getline(ifs,line)){
+        std::istringstream ss(line);
+        uint64_t timestamp;
+        uint64_t bytes;
+        char comma;
+        if (ss >> timestamp >> comma >> bytes) {
+            if (comma == ',') {
+                profile_data.push_back(RawProfilePoint{.timestamp = timestamp,.bytes=bytes});
+            }
+        }
+        else if (!line.empty()) {
+            std::cerr << "Warning: Skipping malformed line: " << line << std::endl;
+        }
+    }
+    ifs.close();
+
+    uint64_t prev_bytes = 0;
+    uint64_t cur_bytes = 0;
+    uint64_t timestamp0 = 0;
+    for(auto& point:profile_data){
+        if(point.bytes == 0){
+            break;
+        }
+        if(cur_bytes == point.bytes){
+            continue;
+        }
+        else{
+            prev_bytes = cur_bytes;
+            cur_bytes = point.bytes;
+            double gbs = (cur_bytes - prev_bytes) / 1.0 / (point.timestamp - timestamp0);
+            timestamp0 = point.timestamp;
+            point.bw = gbs;
+        }
+    }
+    auto start_timestamp = profile_data[0].timestamp;
+
+    auto filtered = profile_data | std::views::filter([](RawProfilePoint point){return point.bw > 0.00000001;})
+                                | std::views::transform([start_timestamp](RawProfilePoint point){point.timestamp -= start_timestamp;return point;});
+
+    // 处理后的数据写入文件方便使用python制作成perfetto文件或者可视化
+    std::string processed_peofile_file_name = std::string("data_")+nic_name+std::string("_rx_processed.txt");
+    std::fstream ofs2(processed_peofile_file_name, std::ios::out | std::ios::trunc);
+    if(ofs){
+        for(auto point:filtered){
+            ofs2<<point.timestamp<<","<<point.bw<<"\n";
+        }
+    }
+    ofs2.close();
+
+
+    // (TODO.)打印一下更多summary信息
+    utils::println("收集数据已经存储在:",processed_peofile_file_name,"可以使用python做可视化");
 }
-
-
 
 int main() {
     signal(SIGINT,signal_handler);
@@ -78,7 +149,6 @@ int main() {
                         std::vector<fs::directory_entry>{fs::directory_iterator("/sys/class/infiniband/"),
                                 fs::directory_iterator{}});
     utils::println("NICS:",nics);
-
 
     for(auto nic_name:nics){
         threads.emplace_back(profile_nic_rx,nic_name);
